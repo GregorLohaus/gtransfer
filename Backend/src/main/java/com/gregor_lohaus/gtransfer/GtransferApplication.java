9a46cd08147c48c30c32e12a29f0450afd60d99e
Gregor Lohaus <gregor.lohaus@8mylez.com>
2026-02-25 17:28:11 +0100

    remove conditional from filecleanup service for native iamge compatibility

--- Backend/src/main/java/com/gregor_lohaus/gtransfer/GtransferApplication.java
+++ Backend/src/main/java/com/gregor_lohaus/gtransfer/GtransferApplication.java
@@ -1,5 +1,6 @@
 package com.gregor_lohaus.gtransfer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportRuntimeHints;
@@ -9,11 +10,17 @@ import com.gregor_lohaus.gtransfer.config.ConfigRuntimeHints;
 import com.gregor_lohaus.gtransfer.model.ModelRuntimeHints;
 import com.gregor_lohaus.gtransfer.native_image.HibernateRuntimeHints;
 import com.gregor_lohaus.gtransfer.native_image.WebRuntimeHints;
+import com.gregor_lohaus.gtransfer.services.filecleanup.FileCleanupService;
+
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @RestController
+@EnableScheduling
 @ImportRuntimeHints({ConfigRuntimeHints.class, HibernateRuntimeHints.class, ModelRuntimeHints.class, WebRuntimeHints.class})
 public class GtransferApplication {
+	@Autowired
+	private FileCleanupService cleanupService;
 	public static void main(String[] args) {
 		SpringApplication.run(GtransferApplication.class, args);
 	}
--- Backend/src/main/java/com/gregor_lohaus/gtransfer/services/filewriter/StorageServiceConfiguration.java
+++ Backend/src/main/java/com/gregor_lohaus/gtransfer/services/filewriter/StorageServiceConfiguration.java
@@ -5,12 +5,10 @@ import java.nio.file.Path;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 import com.gregor_lohaus.gtransfer.config.types.StorageServiceType;
 
 @Configuration
-@EnableScheduling
 public class StorageServiceConfiguration {
 
     //TODO S3 implementation
