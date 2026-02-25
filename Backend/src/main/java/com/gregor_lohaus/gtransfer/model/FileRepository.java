c5f312b33fe8e1183dc6ac46872664ab2d351b7c
Gregor Lohaus <gregor.lohaus@8mylez.com>
2026-02-25 15:10:34 +0100

    scheduled cleanup

--- Backend/src/main/java/com/gregor_lohaus/gtransfer/model/FileRepository.java
+++ Backend/src/main/java/com/gregor_lohaus/gtransfer/model/FileRepository.java
@@ -1,8 +1,16 @@
 package com.gregor_lohaus.gtransfer.model;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FileRepository extends JpaRepository<File, String> {
+
+    @Query("SELECT f FROM File f WHERE f.expireyDateTime IS NOT NULL AND f.expireyDateTime < :now")
+    List<File> findExpired(@Param("now") LocalDateTime now);
 }
